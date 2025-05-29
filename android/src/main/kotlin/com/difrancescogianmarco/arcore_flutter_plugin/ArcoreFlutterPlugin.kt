74ef1af24ce2bf7802561e352ed1471daaf0f13e
ausabbir76 <99824298+ausabbir76@users.noreply.github.com>
2025-05-29 12:17:46 +0600

    Update ArcoreFlutterPlugin.kt

--- android/src/main/kotlin/com/difrancescogianmarco/arcore_flutter_plugin/ArcoreFlutterPlugin.kt
+++ android/src/main/kotlin/com/difrancescogianmarco/arcore_flutter_plugin/ArcoreFlutterPlugin.kt
@@ -1,52 +1,43 @@
 package com.difrancescogianmarco.arcore_flutter_plugin
 
-import androidx.annotation.NonNull
-import androidx.annotation.Nullable
+import android.app.Activity
+import android.content.Context
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.PluginRegistry.Registrar
 
 class ArcoreFlutterPlugin : FlutterPlugin, ActivityAware {
 
-    @Nullable
-    private var flutterPluginBinding: FlutterPlugin.FlutterPluginBinding? = null
-    
-    private var methodCallHandler: MethodCallHandlerImpl? = null
-
     companion object {
-        const val TAG = "ArCoreFlutterPlugin"
-        private const val CHANNEL_NAME = "arcore_flutter_plugin"
+        lateinit var context: Context
+        lateinit var activity: Activity
+        lateinit var channel: MethodChannel
+
+        // Optional if you want to keep support for V1 registrars (not needed unless you're supporting old versions)
+        @JvmStatic
+        fun registerWith(registrar: Registrar) {
+            context = registrar.context()
+            activity = registrar.activity()
+        }
     }
 
-    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-        this.flutterPluginBinding = flutterPluginBinding
+    override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
+        context = binding.applicationContext
     }
 
-    override fun onDetachedFromEngine(p0: FlutterPlugin.FlutterPluginBinding) {
-        this.flutterPluginBinding = null
-    }
-
-
-    override fun onDetachedFromActivity() {
-        //TODO remove othen channel
-        methodCallHandler?.stopListening()
-        methodCallHandler = null
-    }
-
-    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
-        onAttachedToActivity(binding)
-    }
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {}
 
     override fun onAttachedToActivity(binding: ActivityPluginBinding) {
-        flutterPluginBinding?.platformViewRegistry?.registerViewFactory(CHANNEL_NAME, ArCoreViewFactory(binding.activity, flutterPluginBinding?.binaryMessenger!!))
-        methodCallHandler = MethodCallHandlerImpl(
-                binding.activity, flutterPluginBinding?.binaryMessenger!!)
-
+        activity = binding.activity
     }
 
-    override fun onDetachedFromActivityForConfigChanges() {
-        onDetachedFromActivity()
+    override fun onDetachedFromActivityForConfigChanges() {}
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        activity = binding.activity
     }
 
-
+    override fun onDetachedFromActivity() {}
 }
